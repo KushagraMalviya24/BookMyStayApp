e977d22cd6d04fa5c04d9a9a9435f92c3af628cb
KushagraMalviya24 <kushagramalviya24@gmail.com>
2026-03-16 09:06:02 +0530

    Added Use Case 2

---
+++ src/U2.java
@@ -0,0 +1,105 @@
+/**
+ * =============================================================
+ * ABSTRACT CLASS - Room
+ * =============================================================
+ * Use Case 2: Basic Room Types & Static Availability
+ * @version 2.1
+ */
+
+abstract class Room {
+
+    protected int numberOfBeds;
+    protected int squareFeet;
+    protected double pricePerNight;
+
+    public Room(int numberOfBeds, int squareFeet, double pricePerNight) {
+        this.numberOfBeds = numberOfBeds;
+        this.squareFeet = squareFeet;
+        this.pricePerNight = pricePerNight;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Beds: " + numberOfBeds);
+        System.out.println("Size: " + squareFeet + " sqft");
+        System.out.println("Price per night: " + pricePerNight);
+    }
+}
+
+/**
+ * =============================================================
+ * CLASS - SingleRoom
+ * =============================================================
+ * Represents a single room in the hotel.
+ * @version 2.1
+ */
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super(1, 250, 1500.0);
+    }
+}
+
+/**
+ * =============================================================
+ * CLASS - DoubleRoom
+ * =============================================================
+ * Represents a double room in the hotel.
+ * @version 2.1
+ */
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super(2, 400, 2500.0);
+    }
+}
+
+/**
+ * =============================================================
+ * CLASS - SuiteRoom
+ * =============================================================
+ * Represents a suite room in the hotel.
+ * @version 2.1
+ */
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super(3, 750, 5000.0);
+    }
+}
+
+/**
+ * =============================================================
+ * MAIN CLASS - UseCase2RoomInitialization
+ * =============================================================
+ * Demonstrates room initialization and static availability.
+ * @version 2.1
+ */
+public class U2 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hotel Room Initialization\n");
+
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
+
+        int singleAvailability = 5;
+        int doubleAvailability = 3;
+        int suiteAvailability = 2;
+
+        System.out.println("Single Room:");
+        singleRoom.displayRoomDetails();
+        System.out.println("Available: " + singleAvailability);
+        System.out.println();
+
+        System.out.println("Double Room:");
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available: " + doubleAvailability);
+        System.out.println();
+
+        System.out.println("Suite Room:");
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available: " + suiteAvailability);
+    }
+}
