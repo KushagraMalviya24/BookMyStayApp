51f7a3fc56f57883ae4c41d7023ecd364f528754
KushagraMalviya24 <kushagramalviya24@gmail.com>
2026-03-16 08:47:30 +0530

    U1: Application entry and welcome message

---
+++ src/U1.java
@@ -0,0 +1,28 @@
+/**
+ * U1 - Application Entry & Welcome Message
+ *
+ * This class represents the entry point of the
+ * Book My Stay - Hotel Booking Management System.
+ *
+ * It demonstrates how a Java application starts
+ * execution and prints a welcome message.
+ *
+ * @author Vimal Raj
+ * @version 1.0
+ */
+
+public class U1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("=======================================");
+        System.out.println("            Book My Stay App");
+        System.out.println("       Hotel Booking Management");
+        System.out.println("=======================================");
+
+        System.out.println("Version : 1.0");
+        System.out.println("Welcome to the Hotel Booking System!");
+
+        System.out.println("Application started successfully.");
+    }
+}
